6d8a56b462c8ffdac7e7df9d1b672eda2a958baa
JavierLopezC <javier.lopezcano@estudiante.uam.es>
2018-02-22 17:58:33 +0100

    soper pipes casi acabado

--- Practica1/Semana 2/Ejercicio9.c
+++ Practica1/Semana 2/Ejercicio9.c
@@ -40,14 +40,15 @@ double sumaAbsoluta(int a, int b){
 }
 	
 int	main(void){	
-	int	fd[2], fe[2], ff[2], fg[2], fh[2], fi[2], fj[2], fk[2], nbytes;
+	int	fd[2], fe[2], ff[2], fg[2], fh[2], fi[2], fj[2], fk[2];
 	int pipe_status1, pipe_status2, pipe_status3, pipe_status4, pipe_status5, pipe_status6, pipe_status7, pipe_status8;
-	int a1, b1, a2, b2, a3, b3, a4, b4;	
+	int a, b;	
 	pid_t pid1, pid2, pid3, pid4;	
-	char string[] = "2,3";	
-	char readbuffer[80], readbuffer1[80], readbuffer2[80], readbuffer3[80], readbuffer4[80];
-	double result;			
-		
+	char string[20];	
+	char readbuffer[80];
+	double result;
+
+	/*Creamos las pipes*/			
 	pipe_status1 = pipe(fd);
 	if(pipe_status1 == -1){	
 		perror("Error creando la tuberia\n");	
@@ -95,7 +96,12 @@ int	main(void){
 		perror("Error creando la tuberia\n");	
 		exit(EXIT_FAILURE);	
 	}	
-		
+
+	/*Pedimos por pantalla los valores de O1 y O2*/
+	printf("Introduzca 2 enteros separados por una coma: ");
+	scanf("%s", string);
+
+	/*Hacemos los fork para lanzar los procesos hijo*/	
 	if((pid1 = fork())	==	-1){	
 		perror("Error al hacer fork");	
 		exit(EXIT_FAILURE);	
@@ -111,83 +117,117 @@ int	main(void){
 		exit(EXIT_FAILURE);	
 	}
 
+
 	if(pid1 != 0 && pid2 != 0 && pid3 != 0 && ((pid4 = fork())	==	-1)){	
 		perror("Error al hacer fork");	
 		exit(EXIT_FAILURE);	
 	}
-		
+	
+	/*El primer hijo realiza la potencia*/	
 	if(pid1 == 0){		
-		close(fd[1]);	
-		nbytes = read(fd[0], readbuffer, sizeof(readbuffer));
-		printf("PID = %d.\tHe recibido el string: %s\n", getpid(), readbuffer);	
-		a1=atoi(strtok(readbuffer, ","));
-		b1=atoi(strtok(NULL, ","));
-		result=potencia(a1, b1);
-		sprintf(readbuffer, "Resultado de la potencia: %lf\n", result);
+		close(fd[1]);
+		/*Leer los datos del pipe e imprimirlos*/	
+		read(fd[0], readbuffer, sizeof(readbuffer));
+		printf("PID = %d.\tHe recibido el string: %s", getpid(), readbuffer);	
+		/*Separar la cadena obtenida para separar los 2 datos y pasarlos a ints*/
+		a=atoi(strtok(readbuffer, ","));
+		b=atoi(strtok(NULL, ","));
+		/*Calcular la operación*/
+		result=potencia(a, b);
+		/*Crear la cadena con el resultado*/
+		sprintf(readbuffer, "Resultado de la potencia: %lf", result);
 		close(fh[0]);
+		/*Escribir la nueva cadena en el pipe*/
 		write(fh[1], readbuffer, strlen(readbuffer));
 		exit(0);	
+	/*El segundo hijo realiza el factorial*/	
 	}else if(pid2 == 0){	
 		close(fe[1]);	
-		nbytes = read(fe[0], readbuffer, sizeof(readbuffer));
+		/*Leer los datos del pipe e imprimirlos*/	
+		read(fe[0], readbuffer, sizeof(readbuffer));
 		printf("PID = %d.\tHe recibido el string: %s\n", getpid(), readbuffer);
-		a2=atoi(strtok(readbuffer, ","));
-		b2=atoi(strtok(NULL, ","));	
-		result=factorial(a2, b2);
-		sprintf(readbuffer, "Resultado de O1!/O2: %lf\n", result);
+		/*Separar la cadena obtenida para separar los 2 datos y pasarlos a ints*/
+		a=atoi(strtok(readbuffer, ","));
+		b=atoi(strtok(NULL, ","));	
+		/*Calcular la operación*/
+		result=factorial(a, b);
+		/*Crear la cadena con el resultado*/
+		sprintf(readbuffer, "Resultado de O1!/O2: %lf", result);
 		close(fi[0]);
+		/*Escribir la nueva cadena en el pipe*/
 		write(fi[1], readbuffer, strlen(readbuffer));
 		exit(0);
+	/*El tercer hijo realiza el número combinatorio*/ 
 	}else if(pid3 == 0){	
 		close(ff[1]);	
-		nbytes = read(ff[0], readbuffer, sizeof(readbuffer));	
+		/*Leer los datos del pipe e imprimirlos*/	
+		read(ff[0], readbuffer, sizeof(readbuffer));	
 		printf("PID = %d.\tHe recibido el string: %s\n", getpid(), readbuffer);
-		a3=atoi(strtok(readbuffer, ","));
-		b3=atoi(strtok(NULL, ","));	
-		result=numCombinatorio(a3, b3);
-		sprintf(readbuffer, "Resultado del numero combinatorio: %lf\n", result);
+		/*Separar la cadena obtenida para separar los 2 datos y pasarlos a ints*/
+		a=atoi(strtok(readbuffer, ","));
+		b=atoi(strtok(NULL, ","));	
+		/*Calcular la operación*/
+		result=numCombinatorio(a, b);
+		/*Crear la cadena con el resultado*/
+		sprintf(readbuffer, "Resultado del numero combinatorio: %lf", result);
 		close(fj[0]);
+		/*Escribir la nueva cadena en el pipe*/
 		write(fj[1], readbuffer , strlen(readbuffer));
 		exit(0);
+	/*El último hijo realiza la suma de valores absolutos*/
 	}else if(pid4 == 0){
 		close(fg[1]);	
-		nbytes = read(fg[0], readbuffer, sizeof(readbuffer));	
+		/*Leer los datos del pipe e imprimirlos*/	
+		read(fg[0], readbuffer, sizeof(readbuffer));	
 		printf("PID = %d.\tHe recibido el string: %s\n", getpid(), readbuffer);
-		a4=atoi(strtok(readbuffer, ","));
-		b4=atoi(strtok(NULL, ","));	
-		result=sumaAbsoluta(a4, b4);
-		sprintf(readbuffer, "Resultado de la suma de valores absolutos: %lf\n", result);
-		close(fj[0]);
-		write(fj[1], readbuffer , strlen(readbuffer));
-		exit(0);	
+		/*Separar la cadena obtenida para separar los 2 datos y pasarlos a ints*/
+		a=atoi(strtok(readbuffer, ","));
+		b=atoi(strtok(NULL, ","));
+		/*Calcular la operación*/	
+		result=sumaAbsoluta(a, b);
+		/*Crear la cadena con el resultado*/
+		sprintf(readbuffer, "Resultado de la suma de valores absolutos: %lf", result);
+		close(fk[0]);
+		/*Escribir la nueva cadena en el pipe*/
+		write(fk[1], readbuffer , strlen(readbuffer));
+		exit(0);
+	/*El proceso padre escribe los datos y lee el resultado de las pipes*/	
 	}else{	
-		/* Cierre del descriptor de salida en el padre*/	
+		/* Cierre de los descriptores de salida en el padre*/	
 		close(fd[0]);
 		close(fe[0]);
 		close(ff[0]);
 		close(fg[0]);
+
+		/*Cierre de los descriptores de entrada del padre*/
 		close(fh[1]);
 		close(fi[1]);
 		close(fj[1]);
 		close(fk[1]);	
-		/*Leer algo de la tubería... el saludo! */
+
+		/*Escribir en las tuberías la sitring con O1 y O2*/
 		write(fd[1], string, strlen(string));
 		write(fe[1], string, strlen(string));	
 		write(ff[1], string, strlen(string));
 		write(fg[1], string, strlen(string));
 
+		/*Leer de las tuberías las stings con los resultados e imprimirlas*/
 
-		nbytes = read(fh[0], readbuffer1, sizeof(readbuffer1));	
-		printf("\n%s\n", readbuffer1);
+		/*Potencia*/
+		read(fh[0], readbuffer, sizeof(readbuffer));	
+		printf("\n%s\n", readbuffer);
 
-		nbytes = read(fi[0], readbuffer2, sizeof(readbuffer2));
-		printf("%s\n", readbuffer2);
+		/*Factorial*/
+		read(fi[0], readbuffer, sizeof(readbuffer));
+		printf("%s\n", readbuffer);
 
-		nbytes = read(fj[0], readbuffer3, sizeof(readbuffer3));
-		printf("%s\n", readbuffer3);
+		/*Número combinatorio*/
+		read(fj[0], readbuffer, sizeof(readbuffer));
+		printf("%s\n", readbuffer);
 
-		nbytes = read(fk[0], readbuffer4, sizeof(readbuffer4));	
-		printf("%s\n", readbuffer4);
+		/*Suma de valores absolutos*/
+		read(fk[0], readbuffer, sizeof(readbuffer));	
+		printf("%s\n", readbuffer);
 
 	}
 
